1ddf94f8064bcc817cd8b332768b49f923954503
Connor Settle <csettle@stanford.edu>
2018-07-13 11:08:52 -0700

    Fixed bugs

--- Examples/radio_lib_test/radio_lib_test.ino
+++ Examples/radio_lib_test/radio_lib_test.ino
@@ -1,28 +1,28 @@
 #include <ax25.h>
 
-#define KICKSAT_RADIO_SS        10
+#define KICKSAT_RADIO_SS        6
 #define KICKSAT_RADIO_INTERUPT  2
-#define KICKSAT_RADIO_SDN       9
-#define TEENSY_LED              13
+#define KICKSAT_RADIO_SDN       3
+#define TEENSY_LED              9
 #define RH_RF22_HAVE_SERIAL
 
 AX25 radio = AX25(KICKSAT_RADIO_SS, KICKSAT_RADIO_INTERUPT);
 
 void setup() {
-  Serial.begin(9600);
+  SerialUSB.begin(9600);
   pinMode(TEENSY_LED, OUTPUT);
   pinMode(KICKSAT_RADIO_SDN, OUTPUT);
-  Serial.write("Testing 123");
+  SerialUSB.write("Testing 123");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(TEENSY_LED, HIGH);
-  Serial.println("sending");
-  radio.powerAndInit(KICKSAT_RADIO_SDN);
-  radio.transmit("FFF");
+  SerialUSB.println("sending");
+  radio.powerAndInit();
+  radio.transmit("FFF", 12);
   digitalWrite(TEENSY_LED, LOW);
 
-  delay(1000);
+  delay(100);
 
 }
